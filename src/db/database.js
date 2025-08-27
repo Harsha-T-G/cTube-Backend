d96084c3bbd4f1402b4eb6f1aefb2c56ed88f02e
Harsha <harshatg2004@gmail.com>
2025-08-27 19:32:54 +0530

    learn utils error handling

--- src/db/database.js
+++ src/db/database.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
 import { DB_NAME } from "../constants.js";
+import { app } from "../app.js";
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
+    app.on("error", (error) => {
+      console.log("ERR : ", error);
+      throw error;
+    });
     console.log(
       `\n MongoDB connected !! DBHost : ${connectionInstance.connection.host}`
     );
