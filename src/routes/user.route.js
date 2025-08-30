ee11134c951707a8cbe9e7c5822025f28bd25755
Harsha <harshatg2004@gmail.com>
2025-08-30 13:33:40 +0530

    add refershToken

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
@@ -27,6 +28,8 @@ router.route("/login").post(loginUser);
 //secured routes
 
 router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
 
 // router.post("/register", registerUser);
 // router.route("/login").post(loginUser);
